63b1b74e46ed04c65c284727782bb228ba9b4305
Santhoshui <Gsanthoshkumar534@gmail.com>
2020-09-21 18:46:30 +0530

    Add Screenshots and video
    format components

--- src/components/LatestValueSection.tsx
+++ src/components/LatestValueSection.tsx
@@ -6,18 +6,18 @@ import { useSubscription, useQuery } from 'urql';
 import { actions } from '../Features/Metrics/reducer';
 
 interface ILatestValueSectionProps {
-    selectedMetrics: string[];
+  selectedMetrics: string[];
 }
 
 interface IValueCardProps {
-    metricName: string;
-    currentValue: number;
+  metricName: string;
+  currentValue: number;
 }
 
 const ValueCard: React.FC<IValueCardProps> = ({ metricName, currentValue }) => {
-    const [value, setValue] = useState(currentValue);
-    const [result] = useQuery({
-        query: `query ($metricName: String!) {
+  const [value, setValue] = useState(currentValue);
+  const [result] = useQuery({
+    query: `query ($metricName: String!) {
             getLastKnownMeasurement(metricName:$metricName){
               metric
               value
@@ -25,45 +25,44 @@ const ValueCard: React.FC<IValueCardProps> = ({ metricName, currentValue }) => {
               unit
             }
           }`,
-        variables: {
-            metricName
-        }
-    });
-    const { data } = result;
-    useEffect(() => {
-        setValue(data ? data.getLastKnownMeasurement.value : 0)
-    }, [data])
-    return <Grid item md={5} xs={6}>
-        <Card elevation={2}>
-            <CardHeader title={metricName} />
-            <CardContent>
-                <Typography variant="h3">
-                    {currentValue ? currentValue : value}
-                </Typography>
-            </CardContent>
-        </Card>
+    variables: {
+      metricName,
+    },
+  });
+  const { data } = result;
+  useEffect(() => {
+    setValue(data ? data.getLastKnownMeasurement.value : 0);
+  }, [data]);
+  return (
+    <Grid item md={5} xs={6}>
+      <Card elevation={2}>
+        <CardHeader title={metricName} />
+        <CardContent>
+          <Typography variant="h3">{currentValue ? currentValue : value}</Typography>
+        </CardContent>
+      </Card>
     </Grid>
-}
-
+  );
+};
 
 const getLatestValues = (state: IState) => {
-    return state.metrics.latestValue;
-}
+  return state.metrics.latestValue;
+};
 
 interface NewMeasurement {
-    newMeasurement: {
-        at: string;
-        metric: string;
-        value: number;
-        unit: string;
-    }
+  newMeasurement: {
+    at: string;
+    metric: string;
+    value: number;
+    unit: string;
+  };
 }
 
 const LatestValueSection: React.FC<ILatestValueSectionProps> = ({ selectedMetrics }) => {
-    const latestValue = useSelector(getLatestValues);
-    const dispatch = useDispatch();
-    const [result] = useSubscription<NewMeasurement>({
-        query: `
+  const latestValue = useSelector(getLatestValues);
+  const dispatch = useDispatch();
+  const [result] = useSubscription<NewMeasurement>({
+    query: `
         subscription {
             newMeasurement {
                 at
@@ -72,24 +71,21 @@ const LatestValueSection: React.FC<ILatestValueSectionProps> = ({ selectedMetric
                 unit
             }
         }`,
-        pause: selectedMetrics.length === 0
-    })
-    const { data } = result;
+    pause: selectedMetrics.length === 0,
+  });
+  const { data } = result;
 
-    useEffect(() => {
-        data && dispatch(actions.fetchedSingleMetric(data.newMeasurement))
-    }, [data, dispatch])
+  useEffect(() => {
+    data && dispatch(actions.fetchedSingleMetric(data.newMeasurement));
+  }, [data, dispatch]);
 
-    return <>
-        {
-            selectedMetrics.map((metric) => (
-                <ValueCard key={metric}
-                    metricName={metric}
-                    currentValue={latestValue[metric]}
-                />
-            ))
-        }
+  return (
+    <>
+      {selectedMetrics.map(metric => (
+        <ValueCard key={metric} metricName={metric} currentValue={latestValue[metric]} />
+      ))}
     </>
-}
+  );
+};
 
-export default LatestValueSection
+export default LatestValueSection;
